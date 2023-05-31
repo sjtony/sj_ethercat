a35623c5b7857b0ada52ba282f81e1751947e7e4
Bjarne von Horn <vh@igh.de>
2023-05-31 15:43:08 +0200

    Fixes for leap 15.4 kernel flavour

--- devices/igb/igb_ethtool-5.14-ethercat.c
+++ devices/igb/igb_ethtool-5.14-ethercat.c
@@ -2182,7 +2182,10 @@ static int igb_set_phys_id(struct net_device *netdev,
 }
 
 static int igb_set_coalesce(struct net_device *netdev,
-			    struct ethtool_coalesce *ec)
+			    struct ethtool_coalesce *ec,
+			    struct kernel_ethtool_coalesce *kec,
+			    struct netlink_ext_ack *ack)
+
 {
 	struct igb_adapter *adapter = netdev_priv(netdev);
 	int i;
@@ -2238,7 +2241,10 @@ static int igb_set_coalesce(struct net_device *netdev,
 }
 
 static int igb_get_coalesce(struct net_device *netdev,
-			    struct ethtool_coalesce *ec)
+			    struct ethtool_coalesce *ec,
+			    struct kernel_ethtool_coalesce *kec,
+			    struct netlink_ext_ack *ack)
+
 {
 	struct igb_adapter *adapter = netdev_priv(netdev);
 
